608cbd38c7a94bc54cd85038139f411b3f609964
Ilya Zakrevsky <ilya@localhost.localdomain>
2017-07-12 09:42:29 +0300

    Обновлено TODO

--- headers/print.h
+++ headers/print.h
@@ -1,5 +1,5 @@
-#ifndef _PRINT_H_
-#define _PRINT_H_
+#ifndef _DEBUG_PRINT_H_
+#define _DEBUG_PRINT_H_
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -10,7 +10,7 @@
 
 #ifdef ENABLE_DEBUG_MSG
 #define PINF(...) fprintf(stdout, "NOTICE: %s: %d: %s: ", __FILE__, __LINE__, __FUNCTION__); fprintf(stdout, __VA_ARGS__); fprintf(stdout, "\n"); fflush(stdout)
-#define PERR(...) fprintf(stderr, "ERROR: %s: %d: %s: ", __FILE__, __LINE__, __FUNCTION__); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n")
+#define PERR(...) fprintf(stderr, "ERROR: %s: %d: %s: ", __FILE__, __LINE__, __FUNCTION__); fprintf(stderr, __VA_ARGS__); fprintf(stderr, "\n"); perror("ERROR")
 #define PRINT(...) printf(__VA_ARGS__); fflush(stdout)
 #else
 #define PERR(...)
